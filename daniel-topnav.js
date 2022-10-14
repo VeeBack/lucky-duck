b230ad13868626540c8ea23ba52b63205879ad25
dshertz <112414476+dshertz@users.noreply.github.com>
2022-10-14 17:28:12 +0200

    första commiten navbar
    Allergi-ikoner går att klicka i och ur. Flaggan går att toggla mellan sv och eng.

---
+++ daniel-topnav.js
@@ -0,0 +1,7 @@
+function showDropDown(a) {
+    document.getElementById(a).classList.toggle("show");
+  }
+
+  function toggleShowHide(a) {    
+    document.getElementById(a).classList.toggle("hide");
+  }
