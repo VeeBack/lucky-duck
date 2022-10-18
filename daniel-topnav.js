760e4d911510438c6243ea2d5e3e2ae13bc8b516
dshertz <112414476+dshertz@users.noreply.github.com>
2022-10-18 10:17:39 +0200

    Fixat fontawsome-ikoner
    Fixat fontawsome-ikoner och raderat bilder.

--- daniel-topnav.js
+++ daniel-topnav.js
@@ -1,7 +1,10 @@
 function showDropDown(a) {
-    document.getElementById(a).classList.toggle("show");
-  }
+  document.getElementById(a).classList.toggle("showBlock");
 
-  function toggleShowHide(a) {    
-    document.getElementById(a).classList.toggle("hide");
-  }
+  document.getElementById('dropdown-arrow-up').classList.toggle("hide");
+  document.getElementById('dropdown-arrow-down').classList.toggle("hide");
+}
+
+function toggleShowHide(a) {
+  document.getElementById(a).classList.toggle("hide");
+}
