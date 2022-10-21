b4c7bf2b7be74c55aa06a7969bc2cc6dc6eba031
Victor Bäck <36706634+VBDojnaz@users.noreply.github.com>
2022-10-21 11:27:27 +0200

    Ändra publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath: '/lucky-duck',
   transpileDependencies: true,
   configureWebpack: {
     devServer: {
